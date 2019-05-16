968cebbf4f5f7de18646dcc5bd5dbe770c57ae66
caoqinghua <qinghua.cao@shanbay.com>
2019-05-16 19:04:45 +0800

    allow only http:business origin

--- src/middlewares/handleCors.ts
+++ src/middlewares/handleCors.ts
@@ -1,6 +1,6 @@
 import { Req, Res } from '../types';
 export const handleCors = (req: Req, res: Res, next: any) => {
-    res.header('Access-Control-Allow-Origin', 'http://149.28.45.101');
+    res.header('Access-Control-Allow-Origin', 'http://business.tssword.xin');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Content-Type,x-requested-with');
