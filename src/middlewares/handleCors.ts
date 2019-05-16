3843368c2d8c2a687dbc60c7549fdbfa3e8a285e
caoqinghua <qinghua.cao@shanbay.com>
2019-05-16 23:11:32 +0800

    fix: update user

--- src/middlewares/handleCors.ts
+++ src/middlewares/handleCors.ts
@@ -1,7 +1,7 @@
 import { Req, Res } from '../types';
 export const handleCors = (req: Req, res: Res, next: any) => {
-    res.header('Access-Control-Allow-Origin', 'http://business.tssword.xin');
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+    // res.header('Access-Control-Allow-Origin', 'http://business.tssword.xin');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Content-Type,x-requested-with');
--- src/services/customers.service.ts
+++ src/services/customers.service.ts
@@ -234,7 +234,7 @@ export async function updateCustomer(
         },
     });
 
-    if (user.job || (user.job.role !== 'manager' && user.id !== 1))
+    if (!user.job || (user.job.role !== 'manager' && user.id !== 1))
         throw { message: '该用户不是客户经理', code: 400 };
     const customer = await getRepository(Customer).findOne({
         relations: ['manager'],
