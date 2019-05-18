dfa52009a998236bbbfc4a1a5d3bc481a742cefc
caoqinghua <qinghua.cao@shanbay.com>
2019-05-19 01:26:35 +0800

    set build_env

--- src/middlewares/handleCors.ts
+++ src/middlewares/handleCors.ts
@@ -1,7 +1,11 @@
 import { Req, Res } from '../types';
+const build_env = process.env.BUILD_ENV;
+
 export const handleCors = (req: Req, res: Res, next: any) => {
-    res.header('Access-Control-Allow-Origin', 'http://business.tssword.xin');
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.header(
+        'Access-Control-Allow-Origin',
+        build_env === 'development' ? 'http://localhost:8000' : 'http://business.tssword.xin',
+    );
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Content-Type,x-requested-with');
